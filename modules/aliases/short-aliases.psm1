91816043a4fcbfa0abf9e5045a6288b7a4a2479c
bino <bonoself@gmail.com>
2025-09-23 20:36:55 +0900

    .

--- modules/aliases/short-aliases.psm1
+++ modules/aliases/short-aliases.psm1
@@ -13,7 +13,6 @@ function d {
 function dd {
     Invoke-Expression "eza $basedParams --time-style=relative -a $args"
 }
-function ex { exit }
 function re { . $profile }
 function c {
     [CmdletBinding()]
